9db2fb6c6994a0ce32d1950130a5d0bc00199d62
xizeyoupan <44920131+xizeyoupan@users.noreply.github.com>
2020-07-23 18:15:43 +0800

    Refactor

--- app.py
+++ app.py
@@ -1,10 +1,17 @@
+import hashlib
 import os
 import time
-import hashlib
-from flask import Flask, render_template
 
+import httpx
+from flask import (Flask, jsonify, make_response, redirect, render_template,
+                   request)
+# from gevent import monkey
+# from gevent.pywsgi import WSGIServer
 from tinydb import Query, TinyDB
-from flask import request, Response, make_response, redirect
+
+from utils import parse_single_feed
+
+# monkey.patch_all()
 
 
 currentPath = os.path.dirname(os.path.realpath(__file__))
@@ -13,6 +20,43 @@ db = TinyDB(currentPath + '/db.json')
 app = Flask(__name__)
 User = Query()
 user = db.table('User')
+feed = db.table('Feed')
+Feed = Query()
+User_now = ''
+
+
+def store_rss_in_db(rss_list: list):
+    result = []
+    for i in rss_list:
+        if not feed.contains((Feed.link == i['link']) & (Feed.owner == i['owner'])):
+            result.append(i)
+    feed.insert_multiple(result)
+
+
+def get_rss_content(client, url):
+    r = client.get(url)
+    return r.text
+
+
+def gather_rss():
+    username = User_now
+    print(username, 'ddd')
+    if not username:
+        return
+    with httpx.Client() as client:
+
+        feed_all_lsit = []
+
+        for key, vaule in user.search(User.name == username)[0]['rss'].items():
+            for i in vaule:
+                try:
+                    r = get_rss_content(client, i)
+                except Exception as e:
+                    print(e)
+                    continue
+                feed_all_lsit.extend(parse_single_feed(r, key, username))
+        store_rss_in_db(feed_all_lsit)
+        print('over')
 
 
 def check_user_status(username, key):
@@ -34,6 +78,20 @@ def get_key(username: str, time_stamp: int) -> str:
     md5 = hashlib.md5(username.encode(encoding='UTF-8')).hexdigest()
     sha = hashlib.sha256(str(time_stamp).encode(encoding='UTF-8')).hexdigest()
     return md5+sha
+
+
+@app.route('/api')
+def api():
+    username = request.cookies.get('username')
+    key = request.cookies.get('key')
+
+    if not check_user_status(username, key):
+        return 'Permission denied.'
+
+    all_action = ['refresh']
+    action = request.args.get("action")
+    if action not in all_action:
+        return jsonify({"state": "error", "info": 'invalid param.'})
 
 
 @app.route('/')
@@ -80,6 +138,7 @@ def registerCheck():
 
 @app.route('/loginCheck')
 def loginCheck():
+    global User_now
     # TODO:delete cookie
     name = request.args.get('name')
     psd = request.args.get('psd')
@@ -94,16 +153,63 @@ def loginCheck():
         res = make_response(redirect('/article'))
         res.set_cookie('username', name, max_age=3600*24*3)
         res.set_cookie('key', get_key(name, time_stamp), max_age=3600*24*3)
+
+        User_now = name
+        expires = user_account.get('expires')
+        if expires:
+            scheduler.reschedule_job('gather_rss', trigger='interval',
+                                     seconds=int(expires))
+
+            scheduler.resume()
         return res
     else:
         return 'password error.'
 
 
+@app.route('/setting', methods=['GET', 'POST'])
+def setting():
+    username = request.cookies.get('username')
+    key = request.cookies.get('key')
+
+    if not check_user_status(username, key):
+        return 'Permission denied.'
+
+    if request.method == 'GET':
+        return render_template('setting.html', title='设置')
+
+    if request.method == 'POST':
+        # TODO proxy
+        files = request.files.get('file')
+        expires = request.form['expires']
+        if not expires.isdigit():
+            return 'invalid expires'
+        rss = files.readlines()
+        # TODO :categorize
+        # {'name':username,'rss':{'category1':['url1','url2'],
+        # 'category2':['url1','url2'],}}
+        rss_dict = {'Default': [i.decode().replace('\n', '').replace('\r', '')
+                                for i in rss]}
+        user.update({'rss': rss_dict, 'expires': expires},
+                    User.name == username)
+
+        scheduler.reschedule_job('gather_rss', trigger='interval',
+                                 seconds=int(expires))
+        scheduler.resume()
+
+        return 'Update successfully.'
+
+
 if __name__ == "__main__":
     app.debug = True
-    # from gevent.pywsgi import WSGIServer
-    # from gevent import monkey
-    # monkey.patch_all()  # 打上猴子补丁
-    # http_server = WSGIServer(('', 5000), app)
-    # http_server.serve_forever()
-    app.run('0.0.0.0', debug=True)
+    from apscheduler.schedulers.gevent import GeventScheduler
+
+    try:
+        scheduler = GeventScheduler()
+        scheduler.add_job(gather_rss, id='gather_rss')
+        scheduler.start(paused=True)
+        # http_server = WSGIServer(('', 5000), app)
+        # http_server.serve_forever()
+        app.run('0.0.0.0', debug=True)
+
+    except (KeyboardInterrupt, SystemExit):
+        scheduler.shutdown()
---
+++ utils.py
@@ -0,0 +1,78 @@
+import datetime
+import re
+import time
+from urllib import parse
+
+import feedparser
+from bs4 import BeautifulSoup
+
+
+def normalize_whitespace(text):
+    text = re.sub(r'[\n\r\t]', ' ', text)
+    text = re.sub(r' +', ' ', text)
+    return text.strip()
+
+
+def extract_feed(html_content, url=''):
+    if not html_content:
+        return html_content
+    soup = BeautifulSoup(html_content, 'lxml')
+
+    for i in soup.find_all('a'):  # 删除超链接
+        if i.get('href'):
+            del i['href']
+        if i.get('target'):
+            del i['target']
+
+    for i in soup.find_all('img'):  # 处理图片,防盗链
+        if i.get('srcset'):
+            del i['srcset']
+        if i.get('height'):
+            del i['height']
+        if i.get('width'):
+            del i['width']
+
+        i['src'] = '/src/?src=' + \
+            parse.quote(i['src']) + '&url='+parse.quote(url)
+
+    res = [str(i) for i in soup.body.contents]
+    return ''.join(res).replace('\xa0', '').strip()
+
+
+def parse_single_feed(html_text, category, username):
+    d = feedparser.parse(html_text)
+
+    each_rss_list = []
+    for i in d['entries']:
+        article_title = i['title']
+        content = i.get('content')
+        summary = i['summary']
+
+        if content:
+            article_content = content[0]['value'] if len(
+                content[0]['value']) > len(summary) else summary
+        else:
+            article_content = summary
+        link = i['link']
+
+        article_content = normalize_whitespace(article_content)
+        article_content = extract_feed(article_content, link)
+
+        try:
+            published_parsed = int(
+                time.mktime(i['published_parsed']))
+        except (KeyError, TypeError):
+            published_parsed = int(time.time())
+
+        '''
+        rss_dict:{feed_title:str,article_title:str,published_time:int,summary:str,
+                link:str, is_read:bool,is_star:bool,category:str,owner:username}
+        '''
+
+        t = {'feed_title': d.feed.title, 'article_title': article_title,
+             'published_time': published_parsed,
+             'summary': article_content, 'link': link, 'is_read': False,
+             'is_star': False, 'category': category, 'owner': username}
+
+        each_rss_list.append(t)
+    return each_rss_list
